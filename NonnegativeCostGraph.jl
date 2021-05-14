7a7ddce466b57d6de9c6854f42fb6eba4e5a48ec
JHaasIII92 <31041229+JHaasIII92@users.noreply.github.com>
2021-05-14 17:38:26 -0500

    Update NonnegativeCostGraph.jl

--- NonnegativeCostGraph.jl
+++ NonnegativeCostGraph.jl
@@ -1,6 +1,10 @@
 #=
 
+NonnegativeCostGraph.jl
+This julia file incluedes
+* Constructor that returns a graph data type
 
+TODO: Add ploting features
 
 =#
 
@@ -15,15 +19,17 @@ struct graph
     costs
 end
 
-function newGraph(V::Vector{Int},A::Vector{Tuple{Int64, Int64}},c::Vector{Number})
+function newGraph(V::Vector{Int},A::Vector{Tuple{Int64, Int64}},c::Vector)
     if sum(c .< 0) > 0   #verify there are no negative costs
         error("Use only nonnegative costs!")
+    elseif size(c,1) != size(A,1)
+        error("Assign cost to each edge!")
     else
-        AdjMatrix = spzeros(Int, size(A,1), size(V,1))
+        AdjMatrix = spzeros(Int, size(V,1),size(V,1))
         # fill matrix with Arcs (i,j)
-        for i in 1:size(A,1)
-            AdjMatrix[A[i][1], i] = 1
-            AdjMatrix[A[i][2], i] = -1
+        for (i,e) in enumerate(A)
+            AdjMatrix[e[1],e[2]] = c[i]
+            AdjMatrix[e[2],e[1]] = c[i]
         end
         return graph(V,A,AdjMatrix,c)
     end
