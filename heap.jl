0ccdd4cb18ef1a1763fb6bc02b21551ba0d0ed0b
JHaasIII92 <31041229+JHaasIII92@users.noreply.github.com>
2021-05-16 01:02:57 -0500

    Update heap.jl

--- heap.jl
+++ heap.jl
@@ -5,10 +5,15 @@
 
 =#
 
-struct heap
-    i
-    k
+mutable struct heap
+    cost
+    vertice
     last_node
+end
+
+struct node
+    val
+    key
 end
 
 
@@ -32,14 +37,62 @@ function decressKey()
 
 end
 
-function extractMin()
+function extractMin(h::heap)
     # remove min from heap replace with last node
-    # call Heapify
-    # return the min
+    minCost = h.cost[1]
+    minVert = h.vertice[1]
+    min = node(minCost, minVert)
+    if empty(h) == false
+        h.cost[1] = h.cost[h.last_node]
+        h.vertice[1] = h.vertice[h.last_node]
+        h.cost[h.last_node] = minCost
+        h.vertice[h.last_node] = minVert
+        h.last_node -= 1
+        println("Pre ReHeap =>  ", h)
+        # call ReHeap
+        ReHeap(h,1)
+        # return the min
+    end
+    return h, min
 end
 
+function ReHeap(h::heap, root)
+    # root is an index of its location in the arrays
+    tempCost = h.cost[root]
+    tempVertice = h.vertice[root]
+    # check if there are two childeren nodes
+    if (2*root) + 1 <= h.last_node
+        (h.cost[2*root] <= h.cost[(2*root) + 1]) ? minCostArg = 2*root : minCostArg = (2*root) + 1
+        if h.cost[root] > h.cost[minCostArg]
+            h.cost[root] = h.cost[minCostArg]
+            h.vertice[root] = h.vertice[minCostArg]
+            h.cost[minCostArg] = tempCost
+            h.vertice[minCostArg] = tempVertice
+            println(root, " if =>  ", h)
+            ReHeap(h, minCostArg)
+        end
+    # if there is one it is a left child
+    elseif 2*root <= h.last_node
+        minCostArg = 2*root
+        if h.cost[root] > h.cost[minCostArg]
+            h.cost[root] = h.cost[minCostArg]
+            h.vertice[root] = h.vertice[minCostArg]
+            h.cost[minCostArg] = tempCost
+            h.vertice[minCostArg] = tempVertice
+            println(root, " elseif =>  ", h)
+            ReHeap(h, minCostArg)
+        end
+    # no childeren then on a leaf end the recursion
+    else
+        println(root, " else =>  ", h)
+        return h
+    end
+end
 
-function empty()
-
-
+function empty(h::heap)
+    isempty = false
+    if h.last_node == 1
+        isempty = true
+    end
+    return isempty
 end
