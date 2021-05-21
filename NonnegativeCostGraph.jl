98d063d8e946420628db279eb4f303bd3431366f
JHaasIII92 <31041229+JHaasIII92@users.noreply.github.com>
2021-05-21 15:56:43 -0500

    Update NonnegativeCostGraph.jl

--- NonnegativeCostGraph.jl
+++ NonnegativeCostGraph.jl
@@ -15,24 +15,49 @@ using SparseArrays
 struct graph
     nodes::Vector
     arcs::Vector
-    AdjMatrix::Matrix
     costs::Dict
     n::Int
+	AdjMatrix::Function
+	LookUpTable::Function
 end
 
-function newGraph(V::Vector{Int},A::Vector{Tuple{Int64, Int64}},c::Vector)
-    if sum(c .< 0) > 0   #verify there are no negative costs
+function newGraph(V::Vector{Int},A::Vector{Tuple{Int64, Int64}},c::Dict)
+    if sum(values(c) .< 0) > 0   #verify there are no negative costs
         error("Use only nonnegative costs!")
-    elseif size(c,1) != size(A,1)
+    elseif c.count != size(A,1)
         error("Assign cost to each edge!")
     else
-        AdjMatrix = spzeros(Int, size(V,1),size(V,1))
-        # fill matrix with Arcs (i,j)
-        for (i,e) in enumerate(A)
-            AdjMatrix[e[1],e[2]] = c[i]
-            AdjMatrix[e[2],e[1]] = c[i]
-        end
         n = size(V,1)
-        return graph(V,A,AdjMatrix,c,n)
+        return graph(V,A,c,n,getAdjMatrix,getLookUpTable)
     end
+end
+
+
+function getAdjMatrix(arcs, costs)
+	"""
+	g.arcs, g.costs
+	"""
+	AdjMatrix = spzeros(Int, size(V,1),size(V,1))
+	# fill matrix with Arcs (i,j)
+	for arc in arcs
+		AdjMatrix[arc[1],arc[2]] = costs[arc]
+		AdjMatrix[arc[2],arc[1]] = costs[arc]
+	end
+	return AdjMatrix
+end
+
+
+function getLookUpTable(arcs, n)
+	"""
+	g.arcs, g.n
+	"""
+	lookUpTable = spzeros(Int,((n)*(n)),1)
+	for (i,e) in enumerate(arcs)
+        pos = (e[1] - 1)*n + 1
+        while lookUpTable[pos] != 0
+            pos += 1
+        end
+        lookUpTable[pos] = e[2]
+    end
+    return lookUpTable
 end
